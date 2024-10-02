26efeb91867bcec4dafa5860b04e807f5bbda17c
승우 <shrike3040@naver.com>
2024-10-02 17:46:54 +0900

    객체지향적 코드 리팩토링

---
+++ Source/test/Private/Datas/ChartGenerator.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Datas/ChartGenerator.h"
+
+ChartGenerator::ChartGenerator()
+{
+}
+
+ChartGenerator::~ChartGenerator()
+{
+}
