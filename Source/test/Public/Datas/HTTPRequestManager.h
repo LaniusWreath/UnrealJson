7654145421f9ec4308c16a3af17fc4fba19bb700
승우 <shrike3040@naver.com>
2024-10-27 00:39:21 +0900

    모듈 플러그인화 위한 프로젝트 종속성 코드 수정. 모든 Manager 인스턴스 생성 및 관리하는 코어 클래스 작성

---
+++ Source/test/Public/Datas/HTTPRequestManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "HTTPRequestManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST_API UHTTPRequestManager : public UObject
+{
+	GENERATED_BODY()
+	
+};
